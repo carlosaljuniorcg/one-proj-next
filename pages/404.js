32bebe30d38118b230daa2ed8e2abe1331e76f09
carlosaljuniorcg <carlosaljuniorcg@hotmail.com>
2023-08-16 15:01:14 -0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link'
+
+export default function Pagina404() {
+    return (
+        <div>
+            <h1>Você se perder e veio parar na página 404</h1>
+            <Link href="/">Ir para a Home</Link>
+        </div>
+    )
+}
